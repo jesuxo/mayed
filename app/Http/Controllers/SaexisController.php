af56f5ffccb3c7513e5d7b5ef8725f6951ed9d5d
Jesus Celis <geal16ster@gmail.com>
2026-08-25 12:37:19 -0400

    recarga

--- app/Http/Controllers/SaexisController.php
+++ app/Http/Controllers/SaexisController.php
@@ -18,7 +18,7 @@ class SaexisController extends Controller
             if(isset($productos)){
                 foreach ($productos as $prd){
 
-                    if(isset($prd->codprod)){
+                    if(isset($prd->codprod) and $prd->codprod !='RECARGA'){
                         $existen = Saexis::where(['codprod'    => $prd->codprod,
                                                   'codubic'    => $prd->codubic,
                                                   'fk_sucursal'=> $prd->fk_sucursal])
